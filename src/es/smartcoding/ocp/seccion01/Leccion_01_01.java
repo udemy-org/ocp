c40fee52cedbcbe9c276d5ca03f0bc0b7a3314fd
Pep Mendez <jmendez1@xtec.cat>
2017-04-21 13:45:07 +0200

    Old classes removed

---
+++ src/es/smartcoding/ocp/seccion01/Leccion_01_01.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package es.smartcoding.ocp.seccion01;
+
+/**
+ * @author jmendez
+ *
+ */
+public class Leccion_01_01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
