8e984b8ed85210a4a161c496e861c8103ed52b59
Pep Mendez <jmendez1@xtec.cat>
2017-04-25 20:09:16 +0200

    Lecture 3/5 in progress

--- src/es/smartcoding/ocp/seccion03/Leccion_03_05.java
+++ src/es/smartcoding/ocp/seccion03/Leccion_03_05.java
@@ -5,7 +5,24 @@ package es.smartcoding.ocp.seccion03;
 
 /**
  * @author pep
- *
+ * 
+ *         Un patrón de diseño es una solución generalmente aceptada a un
+ *         problema recurrente de diseño de software en un contexto dado.
+ * 
+ *         Estas soluciones que han encontrado otros programadores forman un
+ *         vocabulario que utilizan los programadores para comunicarse entre
+ *         ellos.
+ * 
+ *         En el curso de certificación OCP sólo se tratan dos de los patrones
+ *         creacionales, que gestionan la creación de objetos dentro de las
+ *         aplicaciones: el patrón Singleton y el patrón Immutable.
+ * 
+ *         El patrón Singleton soluciona el problema de cómo diseñar una clase
+ *         para que sólamente se pueda crear un único objeto.
+ * 
+ *         
+ * 
+ * 
  */
 public class Leccion_03_05 {
 
