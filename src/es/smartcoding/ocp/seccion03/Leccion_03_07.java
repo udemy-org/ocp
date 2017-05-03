3a67246ca010a55885c0dc3227729b16764ff932
Pep Mendez <jmendez1@xtec.cat>
2017-05-03 18:32:28 +0200

    Lecture 3 is over. Lecture 4 in progress.

--- src/es/smartcoding/ocp/seccion03/Leccion_03_07.java
+++ src/es/smartcoding/ocp/seccion03/Leccion_03_07.java
@@ -7,17 +7,66 @@ package es.smartcoding.ocp.seccion03;
  * @author pep
  * 
  * 
- * Patron de diseño Factory
+ *         El patrón de diseño creacional Factory resuelve el problema de cómo
+ *         crear objetos aun cuando el tipo concreto de objeto no sea conocido
+ *         hasta en tiempo de ejecución.
+ * 
+ *         El patrón Factoria también conocido como el patrón Method Factory, es
+ *         un patrón creacional que se base en la idea de crear objetos en
+ *         función de un conjunto de parámetros de entrada. Es similar al patrón
+ *         Builder excepto que se centra en proporcionar polimorfismo.
+ * 
+ *         Igual que en el patrón Builder los constructores podrian privados o
+ *         protegidos si la clase Factory fuera una clase interna o estuviera en
+ *         el mismo paquete respectivamente.
+ *         
+ *         EL APARTADO ANTERIOR SE DEJA COMO EJERCICIO.
  *
  */
+
+abstract class Vehiculo {
+}
+
+class Turismo extends Vehiculo {
+}
+
+class Deportivo extends Vehiculo {
+}
+
+class TodoTerreno extends Vehiculo {
+}
+
+class VehiculoFactory {
+	public static Vehiculo getVehiculo(int ocupantes) {
+		Vehiculo vehiculo = null;
+
+		switch (ocupantes) {
+		case 1:
+		case 2:
+			vehiculo = new Deportivo();
+			break;
+		case 3:
+		case 4:
+		case 5:
+			vehiculo = new Turismo();
+			break;
+		default:
+			vehiculo = new TodoTerreno();
+			break;
+		}
+
+		return vehiculo;
+	}
+}
+
 public class Leccion_03_07 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Vehiculo v = VehiculoFactory.getVehiculo(3);
+		System.out.println(v);
 	}
 
 }
--- src/es/smartcoding/ocp/seccion04/Leccion_04_01.java
+++ src/es/smartcoding/ocp/seccion04/Leccion_04_01.java
@@ -3,8 +3,19 @@
  */
 package es.smartcoding.ocp.seccion04;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author pep
+ * 
+ *         Generics y Collections
+ * 
+ *         Como Java es acumulativo, debes recordar como trabajar con la clase
+ *         ArrayList y arrays vistos en el curso OCA.
+ *         
+ *         
  *
  */
 public class Leccion_04_01 {
@@ -13,7 +24,33 @@ public class Leccion_04_01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Este código no deberia presentar ningún problema
+		List<String> list = new ArrayList<>(); 			// Lista vacia
+		list.add("Alfa");								// [ "Alfa" ]
+		list.add("Bravo");								// [ "Alfa", "Bravo" ]
+		String[] array = new String[list.size()];		// array vacio
+		array[0] = list.get(1);							// [ "Bravo" ]
+		array[1] = list.get(0);							// [ "Bravo", "Alfa" ]
+		// Bravo-Alfa-
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + "-");
+		}
+		
+		/*
+		 *  este código demuestra dos aspectos interesantes:
+		 *  
+		 *  1. Si modificamos list2 estamos modificando array2
+		 *  2. El método Arrays.asList() retorna una lista cuyo tamaño no se puede modificar
+		 *  
+		 */
+		String[] array2 = { "Alfa", "Bravo" };
+		List<String> list2 = Arrays.asList(array2);
+		list2.set(1, "Charlie");
+		array2[0] = "Delta";
+		String[] array3 = (String[]) list2.toArray();
+		System.out.println(array2[0] + "-" + array2[1]);
+		// list2.remove(1); // java.lang.UnsupportedOperationException 
 
 	}
 
