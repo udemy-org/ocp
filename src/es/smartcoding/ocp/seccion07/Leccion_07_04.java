166dfa977b6fa24921d9b708beb9a084c0721d87
jmendez <jmendez@d03.informatica.escoladeltreball.org>
2017-06-13 11:49:00 +0200

    Lecture 7/4 submit() in progress

--- src/es/smartcoding/ocp/seccion07/Leccion_07_04.java
+++ src/es/smartcoding/ocp/seccion07/Leccion_07_04.java
@@ -3,6 +3,13 @@
  */
 package es.smartcoding.ocp.seccion07;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 /**
  * @author pep
  * 
@@ -25,16 +32,40 @@ package es.smartcoding.ocp.seccion07;
  *         submit() retorna un objeto java.util.concurrent. Future<V>, o
  *         Future<V> para los amigos que puede utilizarse para conocer el estado
  *         del resultado.
- *
+ * 
  */
 public class Leccion_07_04 {
 
+	private static int counter = 0;
+
 	/**
 	 * @param args
+	 * 
+	 *            La API Concurrency permite hacer tareas avanzadas con hilos
+	 *            sin ni siquera utilizar la clase Thread directamente.
+	 * 
+	 *            El valor de retorno de una Future está determinado por el
+	 *            valor de retorno del método run() de la interfaz Runnable, que
+	 *            como sabes es void. Por lo tanto el método get() en este caso
+	 *            retornará null.
+	 * 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService service = null;
+		try {
+			service = Executors.newSingleThreadExecutor();
+			Future<?> result = service.submit(() -> {
+				for (int i = 0; i < 500; i++)
+					Leccion_07_04.counter++;
+			});
+			result.get(10, TimeUnit.SECONDS);
+			System.out.println("Reached!");
+		} catch (TimeoutException e) {
+			System.out.println("Not reached in time");
+		} finally {
+			if (service != null)
+				service.shutdown();
+		}
 	}
 
 }
