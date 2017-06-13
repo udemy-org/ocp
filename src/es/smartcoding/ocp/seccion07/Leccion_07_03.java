452a2e7952769a2980cf9cc213ec083b772b71fa
Pep <jmendez1@xtec.cat>
2017-06-13 09:28:05 +0200

    Lecture 7/4 submit+future in progress

--- src/es/smartcoding/ocp/seccion07/Leccion_07_03.java
+++ src/es/smartcoding/ocp/seccion07/Leccion_07_03.java
@@ -3,8 +3,23 @@
  */
 package es.smartcoding.ocp.seccion07;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author pep
+ * 
+ *         Concurrencia
+ * 
+ *         ExecutorService
+ * 
+ *         A la hora de trabajar con la API Concurrency lo primero que haremos
+ *         será crear un objeto de tipo ExecutorService. A partir de aquí, basta
+ *         enviar las tareas a realizar. ExecutorService ofrece una serie de
+ *         características como thread pooling, thread scheduling etc. que sería
+ *         muy difícil de implementar por nosostros.
+ * 
+ * 
  *
  */
 public class Leccion_07_03 {
@@ -13,7 +28,45 @@ public class Leccion_07_03 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ExecutorService service = null;
+		try {
+			/*
+			 * Con un single-thread tenemos la garantía que las tareas se haran
+			 * en el orden en que se añaden al ExecutorService, aunque si el
+			 * número de threads es muy elevado, puede no ser el caso.
+			 * 
+			 * Main por su puesto sigue siendo un thread independiente
+			 */
+			service = Executors.newSingleThreadExecutor();
+			System.out.println("En pista de despegue " + Thread.currentThread().getName());
+			service.execute(() -> System.out.println("Cerrando puertas " + Thread.currentThread().getName()));
+			service.execute(() -> System.out.println("Comprobando el sistema " + Thread.currentThread().getName()));
+			service.execute(() -> {
+				for (int i = 0; i < 4; i++)
+					System.out.println("Motor " + i + " OK " + Thread.currentThread().getName());
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			});
+
+			System.out.println("Listo para despegar " + Thread.currentThread().getName());
+		} finally {
+			if (service != null)
+				/*
+				 * Si no cerramos el ExecutorService, el programa no acabará. A
+				 * partir de este momento, es ilegal enviar nuevas tareas. Si
+				 * intentáramos enviar un nuevo proceso se produciría una
+				 * excepción de tipo RejectedExecutionException.
+				 * 
+				 * Por su puesto, el método shutdown() NO para las tareas en
+				 * marcha.
+				 */
+				service.shutdown();
+			System.out.println(service.isShutdown());
+			System.out.println(service.isTerminated());
+		}
 
 	}
 
--- src/es/smartcoding/ocp/seccion07/Leccion_07_04.java
+++ src/es/smartcoding/ocp/seccion07/Leccion_07_04.java
@@ -5,6 +5,26 @@ package es.smartcoding.ocp.seccion07;
 
 /**
  * @author pep
+ * 
+ *         Concurrency
+ * 
+ *         ExecutorService
+ * 
+ *         La interfaz ExecutorService extiende la interfaz Executor de donde
+ *         hereda el método execute(). Una alternativa al método execute() es el
+ *         método submit() que puede hacer todo lo que hace el método execute()
+ *         y además retorna un objeto Future que representa el resultado
+ *         pendiente de la tarea.
+ * 
+ *         Junto con los métodos execute() y submit(), debes conocer los métodos
+ *         invokeAll() e invokeAny() que toman una colección de tareas y de
+ *         forma síncrona los ejecutan todos o el primero que acabe
+ *         respectivamente.
+ * 
+ *         Pero, cómo podemos saber que una tarea ha finalizado? El método
+ *         submit() retorna un objeto java.util.concurrent. Future<V>, o
+ *         Future<V> para los amigos que puede utilizarse para conocer el estado
+ *         del resultado.
  *
  */
 public class Leccion_07_04 {
