81842cf31b481af8a303bccc4bf31ef8de8600fe
Pep <jmendez1@xtec.cat>
2017-06-23 00:09:26 +0200

    Lecture 7/19 RecursiveTask in documentation phase

--- src/es/smartcoding/ocp/seccion07/Leccion_07_19.java
+++ src/es/smartcoding/ocp/seccion07/Leccion_07_19.java
@@ -3,6 +3,12 @@
  */
 package es.smartcoding.ocp.seccion07;
 
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+
 /**
  * @author jmendez
  * 
@@ -56,17 +62,133 @@ package es.smartcoding.ocp.seccion07;
  *         La ForkJoinTask es una entidad parecida a un thread pero más ligera.
  *         Una ForkJoinTask comienza la ejecución cuando se envía a un
  *         ForkJoinPool. Una vez ha comenzado, pondrá en marcha otras subtareas.
- *         
- *
+ * 
+ *         Una ForkJoinTask es como un Future mas ligero. El principal mecanismo
+ *         de coordinación es el método fork(), que arranca la ejecución de
+ *         hilos asíncrona y el método join() que no continúa hasta que se ha
+ *         computado el resultado de la tasca.
+ * 
+ *         Generalmente no se hereda de la clase ForkJoinTask, sino de alguna de
+ *         las clases abstractas que soportan algún estilo particular de proceso
+ *         fork/join. Aquellas tareas que no retornan un valor heredarán de
+ *         RecursiveAction (Son parecidas a Runnable). Si retornan un valor
+ *         heredaran de RecursiveTask (Son parecidas a Callable). Y para
+ *         aquellas acciones que disparan otras acciones heredaran de
+ *         CountedCompleter.
+ * 
+ *         Las tareas ForkJoinTask deberían llevar a cabo relativamente pequeñas
+ *         cantidades de computación. Las tareas grandes deberían dividirse en
+ *         subtascas más pequeñas, normalmente a través de una descomposición
+ *         recursiva. Como regla general, una subtarea debe realizar entre 100 y
+ *         1000 operaciones básicas de computación y por su puesto, deben evitar
+ *         a toda costa los lazos infinitos. Si las subtareas son demasiado
+ *         grandes, entonce el paralelismo puede que no mejore la productividad.
+ *         Y si son demasiado pequeñas, entonces la memoria y la propia
+ *         sobrecarga interna de gestión pueden sobrecargar el sistema.
+ * 
+ * 
  */
+
+class RellenaArrayRecursiveAction extends RecursiveAction {
+
+	// Límite inferior
+	private static final int THRESHOLD = 100;
+
+	/*
+	 * Algunos autores usan 'm' como prefijo de las variable miembro
+	 */
+	private int mInicio;
+	private int mFinal;
+	private Double[] mValores;
+
+	/**
+	 * @param mInicio
+	 * @param mFinal
+	 * @param valores
+	 */
+	public RellenaArrayRecursiveAction(int ini, int fin, Double[] valores) {
+		this.mInicio = ini;
+		this.mFinal = fin;
+		this.mValores = valores;
+	}
+
+	@Override
+	protected void compute() {
+		if (mFinal - mInicio <= THRESHOLD) {
+			for (int i = mInicio; i < mFinal; i++) {
+				mValores[i] = Math.random();
+			}
+		} else {
+			int centro = mInicio + ((mFinal - mInicio) / 2);
+			invokeAll(new RellenaArrayRecursiveAction(mInicio, centro, mValores),
+					new RellenaArrayRecursiveAction(centro, mFinal, mValores));
+		}
+	}
+}
+
+class SumaArrayRecursiveTask extends RecursiveTask<Double> {
+
+	// Límite inferior
+	private static final int THRESHOLD = 100;
+
+	/*
+	 * Algunos autores usan 'm' como prefijo de las variable miembro
+	 */
+	private int mInicio;
+	private int mFinal;
+	private Double[] mValores;
+
+	/**
+	 * @param mInicio
+	 * @param mFinal
+	 * @param valores
+	 */
+	public SumaArrayRecursiveTask(int ini, int fin, Double[] valores) {
+		this.mInicio = ini;
+		this.mFinal = fin;
+		this.mValores = valores;
+	}
+
+	@Override
+	protected Double compute() {
+		Double suma = 0.;
+		if (mFinal - mInicio <= THRESHOLD) {
+			for (int i = mInicio; i < mFinal; i++) {
+				suma += mValores[i];
+			}
+			return suma;
+		} else {
+			int centro = mInicio + ((mFinal - mInicio) / 2);
+			RecursiveTask<Double> tascaIzda = new SumaArrayRecursiveTask(mInicio, centro, mValores);
+			tascaIzda.fork();
+			return new SumaArrayRecursiveTask(centro, mFinal, mValores).compute() + tascaIzda.join();
+		}
+
+	}
+}
+
 public class Leccion_07_19 {
+
+	// Dimension del array
+	private static final int DIM = 10_000;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Double[] ds = new Double[DIM];
+		ForkJoinTask<?> task = new RellenaArrayRecursiveAction(0, ds.length, ds);
+		ForkJoinPool pool = new ForkJoinPool();
+		pool.invoke(task);
 
+		for (Double d : ds) {
+			System.out.println(d);
+		}
+
+		ForkJoinTask<Double> task2 = new SumaArrayRecursiveTask(0, ds.length, ds);
+		ForkJoinPool pool2 = new ForkJoinPool();
+		Double sum = pool2.invoke(task2);
+		System.out.println("Sum: " + sum);
 	}
 
 }
