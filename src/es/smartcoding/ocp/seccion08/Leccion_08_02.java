cafc2b11eb1b6bf3841b493809cef1cfa5cb2703
Pep <jmendez1@xtec.cat>
2017-06-23 22:50:25 +0200

    Lecture 8/3 working with streams in progress

--- src/es/smartcoding/ocp/seccion08/Leccion_08_02.java
+++ src/es/smartcoding/ocp/seccion08/Leccion_08_02.java
@@ -3,12 +3,64 @@
  */
 package es.smartcoding.ocp.seccion08;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  * @author pep
  * 
- * Entradas y salidas
+ *         Entradas y salidas
  * 
- * Introducción a los Streams (java.io)
+ *         Introducción a los Streams (java.io)
+ * 
+ *         Los streams o flujos del paquete java.io nada tienen que ver con los
+ *         Streams del paquete java.util.stream. Los streams de entradas y
+ *         salidas se refieren al acceso a recursos de lectura (input) o
+ *         escritura (output).
+ * 
+ *         Los streams de I/O son flujos de datos. Por ejemplo, cuando vemos una
+ *         película en streaming, la película se va cargando poco a poco,
+ *         habitualmente con un buffer que se va llenando para no tener cortes
+ *         en la proyección.
+ * 
+ *         Los streams I/O pueden estar orientados a bytes (Stream) o a
+ *         caracteres (Reader/Writer). Las FileInputStream y FileReader son
+ *         ejemplos de streams que leen ficheros. La primera lee en forma de
+ *         datos binarios, bytes, y la segunda en forma de caracteres o cadenas
+ *         (String).
+ * 
+ *         En general, las clases del paqueta java.io tienen cierta simetria.
+ *         Por ejemplo, si hay una clase FileInputStream, también debe haber una
+ *         clase FileOutputStream. Las excepciones a la regla son las clases
+ *         PrintWriter y PrintStream que no tienen contrapartida.
+ * 
+ *         Los streams o flujos de datos pueden ser de bajo o alto nivel. Los
+ *         streams de bajo nivel conectan directamente con el origen de datos.
+ *         Por ejemplo, la clase FileInputStream lee datos de byte en byte.
+ * 
+ *         Paralelamente, los flujos de alto nivel se construyen encima de otro
+ *         stream mediante un wrapping o clase envolvente. Por ejemplo, el
+ *         constructor de la clase BufferedReader toma como argumento un Reader
+ *         como FileReader.
+ * 
+ *         Al envolver una clase de un stream de bajo nivel en una clase de
+ *         stream de alto nivel, conseguimos funcionalidades adicionales así
+ *         como transformaciones automáticas de los datos.
+ * 
+ *         De la misma manera, un stream de alto nivel puede llevar como
+ *         argumento otro stream de alto nivel.
+ * 
+ *         La libería java.io define cuatro clases abstractas de las que
+ *         descienden todos los streams: InputStream, OutputStream, Reader y
+ *         Writer.
+ * 
+ *         Durante el examen OCP, deberás tener especial cuidado en algunas
+ *         preguntas trampa que mezclan clases Reader/Writer con
+ *         InputStream/OutputStream.
+ * 
+ *         Consulta la tabla 8.2 del libro de referencia si quieres tener una
+ *         relación completa de las clases de flujos.
  *
  */
 public class Leccion_08_02 {
@@ -17,7 +69,16 @@ public class Leccion_08_02 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * Esto es un ejemplo de un stream de alto nivel (BufferedReader) que
+		 * envuelve a una clase de stream de bajo nivel (FileReader).
+		 */
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("anyfile.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
--- src/es/smartcoding/ocp/seccion08/Leccion_08_03.java
+++ src/es/smartcoding/ocp/seccion08/Leccion_08_03.java
@@ -3,12 +3,41 @@
  */
 package es.smartcoding.ocp.seccion08;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author pep
  * 
- * Entradas y salidas
+ *         Entradas y salidas
  * 
- * Trabajando con streams
+ *         Trabajando con streams
+ * 
+ *         A la hora de trabajar con streams hay algunas procesos que vale la
+ *         pena recordar.
+ * 
+ *         1. Cierre de un stream, en general usaremos un bloque
+ *         try-with-resource que cierra los streams de forma automática.
+ * 
+ *         2. Vaciado (Flushing) de un stream, cuando se escribe a un
+ *         OutputStream, el sistema operativo no nos garantiza que se haga la
+ *         escritura inmediatamemte, ya que puede ser guardada en memoria cache.
+ *         Java proporciona el método flush() que fuerza el volcado de un
+ *         buffer. También, si se invoca el método close() escribe el contenido
+ *         del stream.
+ * 
+ *         3. Marcado (Marking) de un stream, si una clase soporta el marcado,
+ *         puede verificarse llamando al método markSupported(), podemos
+ *         mark(int) una posición para volver a ella con el método reset(). El
+ *         método mark() tiene un parámetro entero que representa el valor
+ *         límite de lectura hasta donde se puede hacer un reset().
+ * 
+ *         4. Salto (skipping) de datos, el método skip(n) nos permite saltar
+ *         los siguientes n bytes de un flujo.
  *
  */
 public class Leccion_08_03 {
@@ -17,7 +46,31 @@ public class Leccion_08_03 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Bloque try-with-resources que se ocupa del cierre automático de un
+		 * recurso.
+		 * 
+		 */
+		try (BufferedReader reader = new BufferedReader(new FileReader(new File("README.md")))) {
+			if (reader.markSupported()) {
+				reader.mark(16);
+				System.out.print((char) reader.read());
+				System.out.print((char) reader.read());
+				System.out.print((char) reader.read());
+				System.out.print((char) reader.read());
+				System.out.print((char) reader.read());
+				reader.reset();
+				reader.skip(2);
+				System.out.print((char) reader.read());
+				System.out.print((char) reader.read());
+				System.out.print((char) reader.read());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
