efe45b14939b9f9bc781ad5fbe5ecd73eee45e63
Pep <jmendez1@xtec.cat>
2017-06-25 11:25:05 +0200

    Lecture 9/2 Paths y Files, page 516.

--- src/es/smartcoding/ocp/seccion09/Leccion_09_01.java
+++ src/es/smartcoding/ocp/seccion09/Leccion_09_01.java
@@ -3,6 +3,14 @@
  */
 package es.smartcoding.ocp.seccion09;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author pep
  * 
@@ -39,7 +47,16 @@ package es.smartcoding.ocp.seccion09;
  *         NIO.2 hace buen uso del patrón Factory: una clase que proporciona
  *         métodos estáticos para crear instancias de otra clase. La clase Paths
  *         (con 's' al final) es justamente eso.
- *
+ * 
+ *         NIO.2 también incluye clases de ayuda como java.nio.file.Files
+ *         diseñadas para trabajar con instancias de la clase Path. Las clases
+ *         de ayuda son similiares a las clases factorias en el sentido que
+ *         también proporcionan un conjunto de métodos estáticos que operan con
+ *         una clase concreta, Path en este caso.
+ * 
+ *         La figura 9.1 del libro de referencia te ayudará a entender cómo
+ *         interactuan las clases de la API NIO.2 con su entorno.
+ * 
  */
 public class Leccion_09_01 {
 
@@ -47,7 +64,54 @@ public class Leccion_09_01 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		/*
+		 * Crear un objeto Path es verdaderamente simple. En este ejemplo, tanto
+		 * path1 como path2 se refieren a la misma ruta relativa (porque no
+		 * empieza con '/')
+		 * 
+		 */
+		Path path1 = Paths.get("temporal/ocp.log");
+		Path path2 = Paths.get("temporal", "ocp.log");
+		URI uri1 = path1.toUri();
+		System.out.println(path1);
+		System.out.println(path2);
+		System.out.println(uri1);
+
+		/*
+		 * El método get() de la clase Paths también toma una URI como
+		 * argumento.
+		 */
+		try {
+			Path path3 = Paths.get(new URI("file:/temporal/ocp.log"));
+			URI uri2 = path3.toUri();
+			System.out.println(path3);
+			System.out.println(uri2);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		Path path4 = FileSystems.getDefault().getPath("temporal/ocp.log");
+		System.out.println(path4);
+
+		/*
+		 * Es posible construir un Path de otro sistema de ficheros
+		 */
+		FileSystem fileSystem;
+		try {
+			fileSystem = FileSystems.getFileSystem(new URI("file:/"));
+			Path path5 = fileSystem.getPath("ocp.log");
+			System.out.println(path5);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * Finalmente, la clase File proporciona el método toPath() para
+		 * convertir un File en un Pah.
+		 */
+		Path path5 = new File("temporal/ocp.log").toPath();
+		System.out.println(path5);
 
 	}
 
--- src/es/smartcoding/ocp/seccion09/Leccion_09_02.java
+++ src/es/smartcoding/ocp/seccion09/Leccion_09_02.java
@@ -3,12 +3,25 @@
  */
 package es.smartcoding.ocp.seccion09;
 
+import java.io.IOException;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author pep
  * 
- * NIO.2 New Input/Output
+ *         NIO.2 New Input/Output
  * 
- * Las clases Paths y Files
+ *         Las clases Paths y Files
+ * 
+ *         Es importante destacar que un objeto Path no es un fichero sino una
+ *         representación de una localización dentro del sistema de ficheros.
+ *         Por este motivo, la mayoría de operaciones sobre Paths puede llevarse
+ *         a cabo tanto si el fichero/directorio realmente existe o no. Por
+ *         ejemplo, recuperar el directorio raíz de una Path no requiere que el
+ *         fichero exista, mientras que si invocamos al método toRealPath() el
+ *         fichero ha de existir o se lanzará una excepción comprobada.
  *
  */
 public class Leccion_09_02 {
@@ -17,8 +30,57 @@ public class Leccion_09_02 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*
+		 * Si el fichero no existiera, se lanzaría una excepción de tipo:
+		 * 
+		 * java.nio.file.NoSuchFileException
+		 * 
+		 */
+		Path path1 = Paths.get("ocp.log");
+		try {
+			/*
+			 * Muchos de los métodos que enteraccionan con ficheros y
+			 * directorios llevan parámetros adicionales en forma de varargs.
+			 * 
+			 * Si bien para el examen OCP no es necesario memorizarlos todos, sí
+			 * que deberías se capaz de reconocer qué significado tienene.
+			 * 
+			 * La tabla 9.1. del libro oficial es un buen punto de referencia de
+			 * estos parámetros opcionales.
+			 * 
+			 * Esta tabla omite las clases enum por simplicidad pero puedes
+			 * consultarlas en el Javadoc.
+			 * 
+			 * java.nio.file.LinkOption.NOFOLLOW_LINKS
+			 * 
+			 * java.nio.file.FileVisitOption.FOLLOW_LINKS
+			 * 
+			 * java.nio.file.StandardCopyOption.ATOMIC_MOVE, COPY_ATTRIBUTES,
+			 * REPLACE_EXISTING
+			 */
+			System.out.println(path1.toRealPath(LinkOption.NOFOLLOW_LINKS));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * Muchos de los métodos de la interfaz Path transforma el camino de
+		 * alguna manera y retornan un nuevo objeto Path, lo que permite
+		 * encadenar llamadas de esta forma:
+		 * 
+		 */
+		System.out.println(Paths.get("/bin/../ocp.log").getParent().normalize().toAbsolutePath());
+
+		/*
+		 * Un objeto Path esta formado por diferentes componentes pero el
+		 * elemento raíz no se incluye en la lista de nombres:
+		 */
+		Path path2 = Paths.get("/user/pep/file.txt");
+		System.out.println("Nombre del path completo: " + path2);
+		for (int i = 0; i < path2.getNameCount(); i++) {
+			System.out.println("El elemento " + i + " es: " + path2.getName(i));
+		}
 	}
 
 }
