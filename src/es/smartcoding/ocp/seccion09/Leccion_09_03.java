75c51b296f9ffdc60ffa19c164b6181d8765b3b8
Pep <jmendez1@xtec.cat>
2017-06-26 09:21:42 +0200

    Lecture 9/3 Files attributes, page 532

--- src/es/smartcoding/ocp/seccion09/Leccion_09_03.java
+++ src/es/smartcoding/ocp/seccion09/Leccion_09_03.java
@@ -3,12 +3,29 @@
  */
 package es.smartcoding.ocp.seccion09;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * @author pep
  * 
- * NIO.2 New Input/Output
+ *         NIO.2 New Input/Output
  * 
- * atributos de ficheros
+ *         atributos de ficheros
+ * 
+ *         La clase Files, además de proporcionar numerosos métodos para crear,
+ *         mover, elimiar o leer un fichero/directorio, también viene provista
+ *         de métodos que trabajan con los atributos de un fichero/directorio
+ *         también llamados 'metadata'.
+ * 
+ *         Por ejemplo, un fichero puede tener una marca de escondido (hidden),
+ *         o de sólo lectura.
+ * 
+ *         No todos los sistemas de archivos trabajan con el mismo conjunto de
+ *         atributos, por ejemplo, los ficheros de algunos sistemas operativos
+ *         no tienen permisos a nivel de usuario y otros sí.
  *
  */
 public class Leccion_09_03 {
@@ -17,8 +34,24 @@ public class Leccion_09_03 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * Los atributos básicos de un fichero/directorio pueden consultarse con
+		 * métodos que son perfectamente intuitivos.
+		 * 
+		 * El método isHidden() lanza una excepción porque en algunos sistemas
+		 * operativos es necesario leer información del propio fichero.
+		 */
+		Path path1 = Paths.get("ocp.log");
+		System.out.println(Files.isDirectory(path1));
+		System.out.println(Files.isRegularFile(path1));
+		System.out.println(Files.isSymbolicLink(path1));
+		try {
+			System.out.println(Files.isHidden(path1));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(Files.isReadable(path1));
+		System.out.println(Files.isExecutable(path1));
 	}
 
 }
