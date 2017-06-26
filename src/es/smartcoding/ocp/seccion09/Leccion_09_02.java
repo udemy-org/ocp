3049ea610adac693f531d4d4f989620aa4950d69
Pep <jmendez1@xtec.cat>
2017-06-26 08:51:17 +0200

    Lecture 9/3 Files attributes in pgoress

--- src/es/smartcoding/ocp/seccion09/Leccion_09_02.java
+++ src/es/smartcoding/ocp/seccion09/Leccion_09_02.java
@@ -3,10 +3,21 @@
  */
 package es.smartcoding.ocp.seccion09;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author pep
@@ -194,10 +205,171 @@ public class Leccion_09_02 {
 		 * Con el método toRealPath(Path) todo cambia. Java comprueba que el
 		 * Path exista dentro del sistema de archivos ya que devuelve una
 		 * referencia a un Path real dentro del sistema de archivos.
+		 * 
+		 * Es similar al método toAbsolutePath() en el sentido que convierte un
+		 * camino relativo a absoluto y además verifica que el fichero
+		 * referenciado existe lanzando una excepción de tipo IOException si no
+		 * fuera el caso.
+		 * 
+		 * Es el único método que soporta la opción NOFOLLOW_LINKS.
+		 * 
+		 * El método toRealPath(Path) lleva a cabo pasos adicionales como
+		 * eliminar elementos redundantes del Path, es decir, llama a
+		 * normalize() implícitamente.
+		 * 
 		 */
 		Path path18 = Paths.get("ocp.log");
 		try {
 			System.out.println(path18.toRealPath(LinkOption.NOFOLLOW_LINKS));
+			/*
+			 * Directorio de trabajo actual
+			 */
+			System.out.println(Paths.get(".").toRealPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * Los nombres de los métodos de la clase Files de NIO.2 son mucho más
+		 * directos que los nombres de la clase java.io.File. Por ejemplo, el
+		 * método mkdir() de la clase File ahora es createDirectory() en la
+		 * clase Files.
+		 */
+		System.out.println(Files.exists(Paths.get("target")));
+		/*
+		 * Dos ficheros pueden ser iguales en términos de nombres pero tener
+		 * distintos contenidos.
+		 * 
+		 * El método isSameFile primero llama al método equals(), si son dos
+		 * cadenas iguales retorna true automáticamente. Ni siquiera mira que
+		 * existan. Pero si el método equals() retorna false entonces localiza
+		 * los ficheros y verica que sean el mismo fichero o directorio. En este
+		 * caso, si no existe alguno de ellos lanza una excepcion de tipo
+		 * IOException.
+		 * 
+		 */
+		try {
+			System.out.println(Files.isSameFile(Paths.get("ocpx.log"), Paths.get("ocpx.log")));
+			System.out.println(Files.isSameFile(Paths.get("ocp.log"), Paths.get("target/../ocp.log")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * Creación de directorios con los métodos createDirectory() y
+		 * createDirectories(). El método createDirectory() lanzará una
+		 * excepción de tipo IOException si el directorio ya existe. El método
+		 * createDirectories() crea un directorio y los directorios intermedios
+		 * que haga falta.
+		 * 
+		 */
+		try {
+			// Files.createDirectory(Paths.get("temporal"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * A diferencia de la clase java.io.File, la clase Files de NIO.2
+		 * proporciona una versión sobrecargada del método copy() que copia
+		 * ficheros y directorios dentro del sistema de archivos.
+		 * 
+		 * El comportamiento es el habitual, el método copy() lanza una
+		 * excepción si el fichero/directorio de origen no existe o si el
+		 * fichero/directorio de destino ya existen excepto si se utiliza la
+		 * opción StandardCopyOption.REPLACE_EXISTING.
+		 * 
+		 */
+		try {
+			Files.copy(Paths.get("temporal"), Paths.get("temporal2"), StandardCopyOption.COPY_ATTRIBUTES);
+			Files.copy(Paths.get("/temporal/system.log"), Paths.get("/temporal2/system.log"),
+					StandardCopyOption.ATOMIC_MOVE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * En este ejemplo, se utiliza una versión sobrecargada del método
+		 * copy() con streams de java.io.
+		 */
+		try (InputStream is = new FileInputStream("ocp.log"); OutputStream out = new FileOutputStream(".")) {
+			Files.copy(is, Paths.get("."));
+			Files.copy(Paths.get("ocp.log"), out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * El método Files.move(Path, Path) mueve o renombra un
+		 * fichero/directorio. El método move() puede aplicarse a directorios
+		 * con contenido pero siempre en la misma unidad, de lo contrario se
+		 * lanzaría una excepción de tipo DirectoryNotEmptyException. Mientras
+		 * que si se trata de directorio vacíos pueden estar en diferentes
+		 * unidades.
+		 * 
+		 */
+		try {
+			Files.move(Paths.get("temporal"), Paths.get("temp"));
+			Files.move(Paths.get("README.back"), Paths.get("README2.back"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * El método Files.delete(Path) elimina un fichero o directorio vacío
+		 * dentro del sistema de archivos. Puede lanzar una excepción comprobada
+		 * de tipo IOException en algunos contextos. Si el path representa una
+		 * directorio no vacío se lanzará una excepción de tipo
+		 * DirectoryNotEmptyException.
+		 * 
+		 * Si el path es un enlace simbólico, se eliminará el enlace simbólico
+		 * pero no el fichero real.
+		 * 
+		 * El método Files.deleteIfExists(Path) es idéntico al método
+		 * Files.delete(Path) excepto en que no lanzará una excepción si el
+		 * fichero/directorio no existe pero sí si el directorio no esta vacío.
+		 */
+		try {
+			Files.delete(Paths.get("temporal2"));
+			Files.deleteIfExists(Paths.get("temporal"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * La API NIO.2 también proporciona métodos para operar con flujos
+		 * (streams) de java.io.
+		 */
+		Path path19 = Paths.get("ocp.log");
+		try (BufferedReader reader = Files.newBufferedReader(path19, Charset.defaultCharset())) {
+			String line = null;
+			while ((line = reader.readLine()) != null)
+				System.out.println(line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Path path20 = Paths.get("temporal/file1.txt");
+		List<String> data = new ArrayList();
+		try (BufferedWriter writer = Files.newBufferedWriter(path20, Charset.forName("UTF-16"))) {
+			writer.write("Java NIO.2 Rocks!!!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * Por último, el método Files.readAllLines() lee todas la líneas de un
+		 * fichero de texto y retorna el resultado como una lista de cadenas.
+		 * 
+		 * Importante tener en cuenta aqui que si el fichero es demasiado grande
+		 * puede llegar a lanzarse una excepción de tipo OutOfMemoryError.
+		 */
+		Path path21 = Paths.get("temporal/file1.txt");
+		try {
+			final List<String> lines = Files.readAllLines(path21);
+			for (String line : lines) {
+				System.out.println(line);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
