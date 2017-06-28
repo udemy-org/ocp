a6a50bbf59591d715db70805128eb79c8d3af257
Pep Mendez <jmendez1@xtec.cat>
2017-06-28 09:33:58 +0200

    Chapter 8 over

--- src/es/smartcoding/ocp/seccion10/Leccion_10_08.java
+++ src/es/smartcoding/ocp/seccion10/Leccion_10_08.java
@@ -3,13 +3,28 @@
  */
 package es.smartcoding.ocp.seccion10;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * @author jmendez
  * 
- * JDBC
+ *         JDBC
  * 
- * Gestión de excepciones
- *
+ *         Gestión de excepciones
+ * 
+ *         Hasta ahora hemos vivido en un mundo perfecto donde no nos hemos
+ *         preocupado en exceso de las posibles excepciones que se pueden lanzar
+ *         trabajando con JDBC.
+ * 
+ *         El método getMessage() retorna una cadena con el problema que ha
+ *         encontrado. El método getSQLState() retorna un código de error SQL.
+ *         Finalmente el método getErrorCode() retorna un código de error
+ *         específico de tu base de datos.
+ * 
  */
 public class Leccion_10_08 {
 
@@ -17,8 +32,18 @@ public class Leccion_10_08 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url = "jdbc:mysql://localhost:3306/ocp";
+		try (Connection conn = DriverManager.getConnection(url, "root", "admin");
+				Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+				ResultSet rs = stmt.executeQuery("select descripcion from eventos")) {
+			while (rs.next()) {
+				System.out.println(rs.getString(2));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getSQLState());
+			System.out.println(e.getErrorCode());
+		}
 
 	}
-
 }
